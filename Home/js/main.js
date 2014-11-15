2360d555d341d47c5520311535d8844b9ba48250
Jeriel Jan del Prado <jerieljan@gmail.com>
2014-11-15 19:58:58 +0800

    Improved the showcase landing page for the website: Added images, rearranged text into proper layouts.

---
+++ Home/js/main.js
@@ -0,0 +1,24 @@
+var seed = Math.floor(Math.random() * 10) % 2;
+
+var elements = [
+    {"element": "#section-1", "images": ['image-01a.jpg', 'image-01b.jpg']},
+    {"element": "#section-2", "images": ['image-02a.jpg', 'image-02b.jpg']},
+    {"element": "#section-3", "images": ['image-03a.jpg', 'image-03b.jpg']}
+];
+$(document).ready(function () {
+    for (item in elements) {
+        $(elements[item].element).css("background-image", "url('../images/" + elements[item].images[seed] + "')");
+    }
+});
+
+var map;
+function initialize() {
+    var mapOptions = {
+        zoom: 18,
+        center: new google.maps.LatLng(14.5632215, 121.1836781)
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
