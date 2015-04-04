32b8a44268ed6389868e267e1c71addf5e356617
Jeriel Jan del Prado <jerieljan@gmail.com>
2015-04-04 17:40:25 +0800

    Updated Holy Week with Good Friday procession, updated sync code to only consider filesize differences, fixed icon parsing for Facebook Open Graph.

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync ./Home/_site/ s3://www.ourtransfi.org --region ap-southeast-1 --acl public-read
+aws s3 sync ./Home/_site/ s3://www.ourtransfi.org --region ap-southeast-1 --acl public-read --size-only --delete
