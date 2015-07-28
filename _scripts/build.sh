2a61b5d813cb219d426be70c06b0e0315686d8ef
Jeriel Jan del Prado <jeriel.delprado@novare.com.hk>
2015-07-28 15:26:44 +0800

    Added echo statements and removed a potentially incorrect cd statement (since the script was called with the path). Fix attempt 4...

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -4,13 +4,19 @@
 # This script is used to prepare the project using Bundler (Ruby)
 # then builds the site and htmlproofs it.
 
+# This script assumes that you are running it while in the project's base directory, not the scripts folder.
+
 DEP="Home"
 
 # Start up Bundler
-cd ..
+echo 'Running bundler'
 bundle install
 
 # Run Jekyll
+
+echo 'Building site with Jekyll...'
 cd ${DEP}
 jekyll build
 htmlproof ./_site
+
+echo 'Build complete.'
